6e1212bd90c1c38175a738a8a22ea8961f79ccc8
jCode319 <jdjack3@gmail.com>
2020-11-11 23:33:20 -0500

    commit tree 830e1df41cb58f0aca2f19ab243942460959417c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,23 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
 
+    post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @hero_name = []
+      @hero_power = []
+      @hero_biography = []
+      @team_heros = params[:team][:heros]
+      @team_heros.each do |hero, skill|
+        @hero_name << skill[:name]
+        @hero_power << skill[:power]
+        @hero_biography << skill[:biography]
+        end
+
+      erb :teams
+    end
 end
